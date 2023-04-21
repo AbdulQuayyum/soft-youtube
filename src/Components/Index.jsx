9ea0ddc0bc9a60df48ba42a6bbaa908fb79f5dd3
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-22 00:20:09 +0100

    Cooking...

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,7 +1,13 @@
+import ChannelCard from "./ChannelCard";
+import ChannelDetail from "./ChannelDetail";
 import Footer from "./Footer";
+import Loader from "./Loader";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
+import SearchFeed from "./SearchFeed";
 import Sidebar from "./Sidebar";
+import VideoCard from "./VideoCard";
+import VideoDetail from "./VideoDetail";
 import Videos from "./Videos";
 
-export { Footer, Navbar, SearchBar, Sidebar, Videos };
+export { ChannelCard, ChannelDetail, Footer, Loader, Navbar, SearchBar, SearchFeed, Sidebar, VideoCard, VideoDetail, Videos };
