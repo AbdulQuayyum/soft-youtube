39f895e2060dc456702718eb0e23327696691950
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-21 04:26:13 +0100

    Cooking, but not a Chef👨🏾‍🍳

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h5>Soft - Youtube Clone | A Youtube clone</h5>
-    </>
-  )
-}
-
-export default App
--- src/Views/Index.jsx
+++
@@ -1,6 +0,0 @@
-import ChannelDetail from "./ChannelDetail";
-import Feed from "./Feed";
-import SearchFeed from "./SearchFeed";
-import VideoDetail from "./VideoDetail";
-
-export { ChannelDetail, Feed, SearchFeed, VideoDetail }
