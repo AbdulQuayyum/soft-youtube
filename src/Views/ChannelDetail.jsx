dbd4779366b3e872fbc0329e9ff21e5c1d03ef2b
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-23 00:57:53 +0100

    Cooking...

--- src/Views/ChannelDetail.jsx
+++ src/Views/ChannelDetail.jsx
@@ -29,8 +29,10 @@ const ChannelDetail = () => {
         <Box minHeight="95vh">
             <Box>
                 <div style={{
-                    height: '300px',
-                    background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+                    height: '370px',
+                    minWidth: "100%",
+                    objectFit: "cover",
+                    backgroundImage: `url(${"https://source.unsplash.com/2400x900/?nature,photography,cats,moon"})`,
                     zIndex: 10,
                 }} />
                 <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
--- src/Views/SearchFeed.jsx
+++ src/Views/SearchFeed.jsx
@@ -7,17 +7,17 @@ import { Videos } from "../Components/Index"
 
 const SearchFeed = () => {
     const [videos, setVideos] = useState(null);
-    const { searchTerm } = useParams();
+    const { SearchTerm } = useParams();
 
     useEffect(() => {
-        FetchData(`search?part=snippet&q=${searchTerm}`)
+        FetchData(`search?part=snippet&q=${SearchTerm}`)
             .then((data) => setVideos(data.items))
-    }, [searchTerm]);
+    }, [SearchTerm]);
 
     return (
         <Box p={2} minHeight="95vh">
             <Typography variant="h4" fontWeight={900} color="white" mb={3} ml={{ sm: "100px" }}>
-                Search Results for <span style={{ color: "#ebedf0" }}>{searchTerm}</span> videos
+                Search Results for <span style={{ color: "#ebedf0" }}>{SearchTerm}</span> videos
             </Typography>
             <Box display="flex">
                 <Box sx={{ mr: { sm: '100px' } }} />
